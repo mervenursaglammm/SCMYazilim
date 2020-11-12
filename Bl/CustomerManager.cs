225c086df105b42ba9c1f54cad94bf70d9f802c8
YALOVASMAK <mervenur.saglam@smak.com.tr>
2020-11-12 13:23:19 +0300

    Bl olusturuldu.

---
+++ Bl/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl
+{
+    public class CustomerManager
+    {
+        //Repository _repo=new 
+        //public Result<Customer> Register(RegisterViewModel registerViewModel)
+        //{
+        //   if(registerViewModel.Email==null)
+        //    {
+        //        int result = Insert(new Customer()
+        //        {
+        //            Email = registerViewModel.Email,
+        //            Password = registerViewModel.Password,
+        //            Repass = registerViewModel.Repass,
+        //            IsAdmin = false,
+        //            IsActive = true,
+        //            Guid = Guid.NewGuid()
+        //        });
+        //    }
+
+        //    else
+        //    {
+        //        return 
+        //    }
+        //}
+    }
+}
---
+++ Bl/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bl
+{
+    public class Result<T> where T : class
+    {
+        public List<string> ErrorMessages { get; set; }
+        public T result { get; set; }
+
+        public Result()
+        {
+            ErrorMessages = new List<string>();
+        }
+    }
+}

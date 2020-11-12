447e8a8f9a6b604c9045c5d20a807be62a021bcf
YALOVASMAK <mervenur.saglam@smak.com.tr>
2020-11-12 13:46:23 +0300

    Son haliyle guncellendi.

---
+++ Entities/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+        [Required, MinLength(6)]
+        public string Repass { get; set; }
+        public DateTime Birthday { get; set; }
+        public string ProfileImage { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+        [Required]
+        public bool IsAdmin { get; set; }
+        [Required]
+        public Guid Guid { get; set; }
+        [Required]
+        public DateTime CreateDate { get; set; }
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+        [Required]
+        public string ModifiedUser { get; set; }
+    }
+}
---
+++ Entities/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, MinLength(6)]
+        public string Password { get; set; }
+        [Required, MinLength(6)]
+        public string Repass { get; set; }
+    }
+}

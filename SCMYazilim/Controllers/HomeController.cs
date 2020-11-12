31aacddb2ea6e07d84e173f69f96c82b65ef5033
YALOVASMAK <mervenur.saglam@smak.com.tr>
2020-11-12 13:38:31 +0300

    Html kismi yuklendi.

---
+++ SCMYazilim/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCMYazilim.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(RegisterViewModel registerViewModel)
+        {
+            //if(ModelState.IsValid)
+            //{
+
+            //    if(registerViewModel.Email==null)
+            //    {
+
+            //    }
+            //}
+
+            return View();
+        }
+    }
+}

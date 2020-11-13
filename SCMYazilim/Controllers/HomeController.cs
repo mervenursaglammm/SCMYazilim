13c1dba4a855b44a1b49e471428a52009fd57b13
Sefa Türk <sefatrk@outlook.com.tr>
2020-11-13 08:40:21 +0300

    route

--- SCMYazilim/Controllers/HomeController.cs
+++ SCMYazilim/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 
 namespace SCMYazilim.Controllers
 {
+    [RoutePrefix("Sefa")]
     public class HomeController : Controller
     {
         private CustomerManager<Customer> customerManager = new CustomerManager<Customer>();
@@ -17,10 +18,12 @@ namespace SCMYazilim.Controllers
         {
             return View();
         }
+        [Route("giris")]
         public ActionResult SignIn()
         {
             return View();
         }
+        [Route("uyelik")]
         public ActionResult SignUp()
         {
             return View();

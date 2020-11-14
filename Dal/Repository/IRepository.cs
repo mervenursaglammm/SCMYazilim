09b0eb8e0beda09fad51c8b8432b8ba4ca26d5b1
Sefa Türk <sefatrk@outlook.com.tr>
2020-11-14 08:35:03 +0300

    .

---
+++ Dal/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Repository
+{
+    interface IRepository<T> where T : class
+    {
+        int Insert(T Obj);
+        int Delete(T Obj);
+        int Update(T Obj);
+        int Save();
+        T Find(Expression<Func<T, bool>> where);
+        List<T> List();
+        List<T> List(Expression<Func<T,bool>> where);
+    }
+}
---
+++ Dal/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Repository
+{
+    class Repository<T> : IRepository<T> where T : class
+    {
+        DatabaseContext databaseContext = new DatabaseContext();
+        public int Delete(T Obj)
+        {
+            databaseContext.Set<T>().Remove(Obj);
+            return Save();
+        }
+
+        public T Find(Expression<Func<T, bool>> where)
+        {
+            return databaseContext.Set<T>().Where(where).FirstOrDefault();
+        }
+
+        public int Insert(T Obj)
+        {
+            databaseContext.Set<T>().Add(Obj);
+            return Save();
+        }
+
+        public List<T> List()
+        {
+            return databaseContext.Set<T>().ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> where)
+        {
+            return databaseContext.Set<T>().Where(where).ToList();
+        }
+
+        public int Save()
+        {
+            return databaseContext.SaveChanges();
+        }
+
+        public int Update(T Obj)
+        {
+            return Save();
+        }
+    }
+}

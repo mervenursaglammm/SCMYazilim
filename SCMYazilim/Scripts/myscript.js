6b7c1414fdd20a978a116e7e2e44b8211e969691
User <User@DESKTOP-T7SEF7T>
2020-11-26 16:30:52 +0300

    authorization sayfasında tarihe göre hesaplama yapıldı.

--- SCMYazilim/Scripts/myscript.js
+++ SCMYazilim/Scripts/myscript.js
@@ -12,7 +12,7 @@
         selected = chkArray.join(',') + ",";
 
         if (selected.length > 1) {
-            alert("Seçtiniz " + selected);
+          // alert("Seçtiniz " + selected);
         } else {
             alert("Lütfen onay kutusundan en az bir tanesini seçiniz.");
         }
@@ -32,11 +32,26 @@
     $("#Hesapla").click(function () {
         getValueUsingClass();
         var deneme = 0;
-
+        
+        var today = new Date();
+        var lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+        //alert(lastDayOfMonth.getDate());
+        var d = new Date();
+        //alert(d.getDate());
+        var tarihFarki = lastDayOfMonth.getDate() - d.getDate() + 1;
+        //alert(tarihFarki);
         $(".chk:checked").each(function () {
             deneme += parseFloat($(this).attr("no"));
         });
-        $("#tutar1").html(deneme);
+        var tutar = deneme * tarihFarki;
+        $("#tutar1").html(tutar + "₺");
+        var aciklama ="("+ d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear() + " tarihinden " + lastDayOfMonth.getDate() + "." + (lastDayOfMonth.getMonth() + 1) + "." + lastDayOfMonth.getFullYear() + " tarihine kadar tutarı " + tutar + "₺'dir.)";
+        $("#aciklama").html(aciklama);
+       // alert(d.getMonth());
+
+       
+
+       
     });
 
 });
